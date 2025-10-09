f9096de575f0ce0f1058a53c0ee3e213c7869cbb
RatneshTripathi07 <ratneshgeetatripathi@gmail.com>
2025-10-09 15:25:22 +0530

    Added UI screenshots in the readme

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,23 +2,6 @@ import { createServer } from 'http';
 import { app } from './app';
 import { env } from './config/environment';
 
-console.log('--- DEBUGGING DATABASE CONNECTION ---');
-if (process.env.DIRECT_URL) {
-  try {
-    const url = new URL(process.env.DIRECT_URL);
-    console.log(`DIRECT_URL host: ${url.host}`);
-    console.log(`DIRECT_URL user: ${url.username}`);
-    console.log(`DIRECT_URL password length: ${url.password.length}`);
-    console.log(`DIRECT_URL path: ${url.pathname}`);
-  } catch (e: any) {
-    console.log('Error parsing DIRECT_URL:', e.message);
-    console.log('DIRECT_URL as a string:', process.env.DIRECT_URL);
-  }
-} else {
-  console.log('DIRECT_URL is not set!');
-}
-console.log('------------------------------------');
-
 const port = env.PORT;
 
 const server = createServer(app);
